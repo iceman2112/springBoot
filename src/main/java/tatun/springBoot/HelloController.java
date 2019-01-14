5901cdb484d9d45e7b0a2d792bf7a9d0f44dfe15
Unknown <tatun.slava@gmail.com>
2019-01-14 19:39:18 +0600

    lesson1

---
+++ src/main/java/tatun/springBoot/HelloController.java
@@ -0,0 +1,16 @@
+package tatun.springBoot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "Hello, " + name;
+    }
+}
